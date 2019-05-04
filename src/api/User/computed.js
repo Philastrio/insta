bf78777b38ab93957760993a2eeb4d0d29f65a1e
HanKang <han_gang@naver.com>
2019-05-04 15:20:17 +0900

    isFollowing, isSelf Complete

---
+++ src/api/User/computed.js
@@ -0,0 +1,33 @@
+import { prisma } from "../../../generated/prisma-client";
+
+export default {
+  User: {
+    fullName: parent => {
+      return `${parent.firstName} ${parent.lastName}`;
+    },
+    isFollowing: async (parent, _, { request }) => {
+      const { user } = request;
+      const { id: parentId } = parent;
+      try {
+        return (exists = await prisma.$exists.user({
+          AND: [
+            {
+              id: user.id
+            },
+            {
+              following_some: { id: parent.id }
+            }
+          ]
+        }));
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    },
+    isSelf: (parent, _, { request }) => {
+      const { user } = request;
+      const { id: parentId } = parent;
+      return user.id === parentId;
+    }
+  }
+};
