ef3005ee7c4f9601446d3583ab1f7dacb9ca9a53
HanKang <han_gang@naver.com>
2019-05-12 07:14:01 +0900

    Kill Fragment Comlete!

--- src/api/Post/Post.js
+++ src/api/Post/Post.js
@@ -2,6 +2,10 @@ import { prisma } from "../../../generated/prisma-client";
 
 export default {
   Post: {
+    files: ({ id }) => prisma.post({ id }).files(),
+    comments: ({ id }) => prisma.post({ id }).comments(),
+    user: ({ id }) => prisma.post({ id }).user(),
+    likes: ({ id }) => prisma.post({ id }).likes(),
     isLiked: (parent, _, { request }) => {
       const { user } = request;
       const { id } = parent;
@@ -22,9 +26,6 @@ export default {
           where: { post: { id: parent.id } }
         })
         .aggregate()
-        .count(),
-    files: ({ id }) => prisma.post({ id }).files(),
-    comments: ({ id }) => prisma.post({ id }).comments(),
-    user: ({ id }) => prisma.post({ id }).user()
+        .count()
   }
 };
