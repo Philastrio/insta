d445be6b95f692536bd25f0978af9b539f589b3e
philastrio <philastrio@gmail.com>
2019-05-17 09:31:28 +0900

    createAccount 수정

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -4,7 +4,17 @@ export default {
   Mutation: {
     createAccount: async (_, args) => {
       const { name, email, firstName = "", lastName = "", bio = "" } = args;
-
+      const exists = await prisma.$exists.user({
+        OR: [
+          {
+            name
+          },
+          {email}
+        ]
+      })
+      if(exists){
+        throw Error("이 아이디 또는 이메일은 이미 존재합니다")
+      }
       await prisma.createUser({
         name,
         email,
