450cfc66726963937d9acda2e14d11b46e3d25c8
philastrio <philastrio@gmail.com>
2019-09-03 20:37:02 +0900

    Uploading to S3

--- src/upload.js
+++ src/upload.js
@@ -1,12 +1,36 @@
 import multer from "multer";
+import multerS3 from "multer-s3";
+import aws from "aws-sdk";
 
-const upload = multer({ dest: "uploads/" });
+const s3 = new aws.S3({
+  accessKeyId: process.env.S3_ACCESS_KEY,
+  secretAccessKey: process.env.S3_SECRET_KEY,
+  region: "ap-northeast-2"
+});
+
+//const upload = multer({ dest: "uploads/" }); 이것은 하드에 쓸때 하는 것임
 //업로드 되는 파일 위치를 나타낸다
+
+const upload = multer({
+  storage: multerS3({
+    s3,
+    bucket: "prismakang",
+    metadata: function(req, file, cb) {
+      cb(null, { fieldName: file.fieldname });
+    },
+    key: function(req, file, cb) {
+      cb(null, Date.now().toString());
+    }
+  })
+});
+
 export const uploadMiddleware = upload.single("file");
 
 export const uploadController = (req, res) => {
   const {
-    file: { path }
+    file: { location }
   } = req;
-  res.json({ path });
+  res.json({ location });
 };
+
+// S3는 path가 아니라 location이라 한다. 
